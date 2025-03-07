11f2a32837371407fc2d808b2d49d44a664156c0
nonolook <looklooknono@outlook.com>
2025-03-07 14:57:32 +0800

    [all] fix

--- hooks/useVerioStakeApy.ts
+++ hooks/useVerioStakeApy.ts
@@ -1,10 +1,9 @@
 import { VerioStakePool } from '@/config/bvaults'
-import { useCurrentChainId } from './useCurrentChainId'
-import { useQuery } from '@tanstack/react-query'
-import { getPC } from '@/providers/publicClient'
-import { abiVerioStakePool } from '@/config/abi'
 import { DECIMAL, DECIMAL_10 } from '@/constants'
-import { fmtPercent } from '@/lib/utils'
+import { getPC } from '@/providers/publicClient'
+import { useQuery } from '@tanstack/react-query'
+import { erc20Abi } from 'viem'
+import { useCurrentChainId } from './useCurrentChainId'
 
 export function useVerioStakeApy() {
   const chainId = useCurrentChainId()
@@ -15,15 +14,17 @@ export function useVerioStakeApy() {
     enabled: Boolean(verioStakePool),
     queryFn: async () => {
       const pc = getPC()
-      const [stakePoolAmount, totalStake] = await Promise.all([
-        pc.readContract({ abi: abiVerioStakePool, address: verioStakePool, functionName: 'getStakePoolAmount' }),
-        pc.readContract({ abi: abiVerioStakePool, address: verioStakePool, functionName: 'getTotalStake' }),
-      ])
-      // Number(formatUnits((stakePoolAmount as BigNumberish) ?? 0)) + Number(formatUnits((totalStake as BigNumberish) ?? 0))
-      const singularityIncentivePerDayInIp = 260n
-      const apy = (singularityIncentivePerDayInIp * DECIMAL * DECIMAL_10 * 365n) / (stakePoolAmount + totalStake)
-      console.info('VerioStakeApy:', fmtPercent(apy, 10))
-      return apy
+      // const [stakePoolAmount, totalStake] = await Promise.all([
+      //   pc.readContract({ abi: abiVerioStakePool, address: verioStakePool, functionName: 'getStakePoolAmount' }),
+      //   pc.readContract({ abi: abiVerioStakePool, address: verioStakePool, functionName: 'getTotalStake' }),
+      // ])
+      // // Number(formatUnits((stakePoolAmount as BigNumberish) ?? 0)) + Number(formatUnits((totalStake as BigNumberish) ?? 0))
+      // const singularityIncentivePerDayInIp = 260n
+      // const apy = (singularityIncentivePerDayInIp * DECIMAL * DECIMAL_10 * 365n) / (stakePoolAmount + totalStake)
+      // console.info('VerioStakeApy:', fmtPercent(apy, 10))
+      // return apy
+      const vipTotalSupply = await pc.readContract({ abi: erc20Abi, functionName: 'totalSupply', address: '0x5267F7eE069CEB3D8F1c760c215569b79d0685aD' })
+      return (280320n * DECIMAL * DECIMAL_10) / vipTotalSupply
     },
   })
 }
