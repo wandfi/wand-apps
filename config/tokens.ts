2cec2eae0537f6d27b13f90e4373f585a1e6becc
nonolook <looklooknono@outlook.com>
2025-05-19 13:39:32 +0800

    [test] update

--- config/tokens.ts
+++ config/tokens.ts
@@ -21,8 +21,8 @@ export const TOKENS: Token[] = [
   { address: '0xADb174564F9065ce497a2Ff8BEC62b21e8b575d6', symbol: 'bvIP', decimals: 18, chain: [story.id] },
   { address: '0x5267F7eE069CEB3D8F1c760c215569b79d0685aE', symbol: 'WIP', decimals: 18, chain: [story.id] },
 
-  { address: '0x2b421fdf25ac7e21fd695ffb7c1ac72df15ca828', symbol: 'YTK', decimals: 18, chain: [sepolia.id] },
-  { address: '0xbf91828752703d8ca59ee509f61d883e8d67a49e', symbol: 'BT-INFRA', decimals: 18, chain: [sepolia.id] },
+  { address: '0xa247358b5e3350354bacd8a15becb218d11a9a59', symbol: 'YTK', decimals: 18, chain: [sepolia.id] },
+  { address: '0xd4b290cbf95aa997f75f12a0aab4084869a10c4e', symbol: 'BT-INFRA', decimals: 18, chain: [sepolia.id] },
 ]
 
 export const TOKENS_MAP: { [k: `${number}_${Address}`]: Token } = TOKENS.reduce((map, item) => {
